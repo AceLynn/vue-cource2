b096299cfab4f0ffab7953c236b377132caaebd2
youyinli <acedanny@qq.com>
2020-07-06 21:20:07 +0800

    feature: mock安装

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Bus from "./lib/bus";
+import axios from "axios";
+
+if (process.env.NODE_ENV !== "production") {
+  require("./mock");
+}
+
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
 Vue.prototype.$bus = Bus;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <p>{{ food }}</p>
 
@@ -28,7 +28,9 @@ export default {
   },
   methods: {
     getInfo() {
-      
+      this.$axios("/getUserInfo").then((res) => {
+        console.log(res);
+      });
     },
   },
 };
