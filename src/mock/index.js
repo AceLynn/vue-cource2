813ce3c2a57dbc6e54d7508513c44fdcc2cc9ba5
acelynn <acelynn@163.com>
2020-07-10 16:41:53 +0800

    part 9: mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,13 @@
-import Mock from "mockjs";
+import Mock, { Random } from "mockjs";
 import { getUserInfo } from "./response/user";
 
 Mock.mock("/getUserInfo", "post", getUserInfo);
+
+Random.extend({
+  fruit() {
+    const fruit = ["apple", "peach", "lemon"];
+    return this.pick(fruit);
+  },
+});
 
 export default Mock;
--- src/mock/response/user.js
+++ src/mock/response/user.js
@@ -1,4 +1,38 @@
+import Mock from "mockjs";
+
+const Random = Mock.Random;
+
 export const getUserInfo = (options) => {
-  console.log(options);
-  return options;
+  // console.log(options);
+  // return options;
+  // 设置 mock 随机方法
+  const template = {
+    "str|2-4": "lison",
+    "name|5": "lison",
+    "age|+2": 18,
+    // 调用 random 生成随机数据
+    email: Random.email(),
+    rang: Random.range(3, 10, 2),
+    data: Random.date("yyyy-MM-dd"),
+    time: Random.time("hh:mm"),
+    datetime: Random.datetime("yyyy-MM-dd hh:mm"),
+    now: Random.now("minute", "yyyy-MM-dd"),
+    img: Random.image("100x200", "#369", "#fff"),
+    color: Random.color(),
+    cword: Random.cword("玩儿索斯岛", 2, 5),
+    cname: Random.cname(),
+    email2: Random.email("lison.com"),
+    region: Random.region(),
+    province: Random.province(),
+    city: Random.city(true),
+    country: Random.county(true),
+    zip: Random.zip(),
+    upperFirstLetter: Random.capitalize("this is lower"),
+    pick: Random.pick([1, 2, 3, 4, 5, 6]),
+    shuffle: Random.shuffle([1, 2, 3, 4, 5, 6]),
+    guid: Random.guid(),
+    id: Random.id(),
+    fruit: Random.fruit(),
+  };
+  return Mock.mock(template);
 };
